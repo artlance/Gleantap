4952cf9734820b4cdb6376cf3d33a55fb2f5fd9a
artlance <artlance.ru@gmail.com>
2021-07-19 13:39:52 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -3224,6 +3224,22 @@ $(document).ready(function () {
         $(this).addClass('blur');
     });
 
+    //-----------------------------------------//
+
+    $('#summernote').summernote({
+        placeholder: 'Hello stand alone ui',
+        tabsize: 2,
+        height: 120,
+        toolbar: [
+            ['style', ['style']],
+            ['font', ['bold', 'underline', 'clear']],
+            ['color', ['color']],
+            ['para', ['ul', 'ol', 'paragraph']],
+            ['table', ['table']],
+            ['insert', ['link', 'picture', 'video']],
+            ['view', ['fullscreen', 'codeview', 'help']]
+        ]
+    });
 
     //-----------------------------------------//
 
