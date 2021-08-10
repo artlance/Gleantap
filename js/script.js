cbb0c6058e20a8ec82958f24b983d1f3e6651e9b
artlance <artlance.ru@gmail.com>
2021-08-10 13:21:16 +0300

    search

--- js/script.js
+++ js/script.js
@@ -3327,6 +3327,27 @@ $(document).ready(function () {
 
     //-----------------------------------------//
 
+    //https://xdsoft.net/jqplugins/autocomplete/
+    var states = [
+        'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+        'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia',
+        'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa',
+        'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland',
+        'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi',
+        'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+        'New Jersey', 'New Mexico', 'New York', 'North Carolina',
+        'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania',
+        'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee',
+        'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington',
+        'West Virginia', 'Wisconsin', 'Wyoming'
+    ];
+
+    $("input#autocomplete").autocomplete({
+        source: [states]
+    });
+
+    //-----------------------------------------//
+
     $(document).on('click', '.cmp-info-plus', function (event) {
         event.preventDefault();
         var thisTemplate = $('#cmp-footer-subject').html();
